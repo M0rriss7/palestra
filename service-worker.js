8a1987e4ca1edf0ce107b4b21ce536326fa104cd
M0rriss7 <maurocalciano2000@gmail.com>
2026-07-17 10:16:43 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "gym-progress-v22";
+const CACHE_NAME = "gym-progress-v23";
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css?v=22",
-  "./app.js?v=22",
-  "./diet.js?v=22",
+  "./style.css?v=23",
+  "./app.js?v=23",
+  "./diet.js?v=23",
   "./manifest.json",
   "./icon.svg"
 ];
